d8e793c5663acb21401562b99982bdb8b2caca59
Yuri Eduardo Lencio <yurilencio@ciandt.com>
2026-05-04 14:46:35 -0300

    subindo testes usuario

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 
 const app = express ()
+app.use(express.json())
+
+const users = []
+
+app.post('/', (req, res) =>{
+    users.push(req.body)
+    console.log(users)
+    res.send("Deu certo")
+})
+
 
 app.get('/', (req, res) => {
-    res.send("Olá. bem vindo :)")
+    res.json(users)
 })
 
 app.listen(8000)
