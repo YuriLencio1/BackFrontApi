0864e812ff0f5cab1c5b6ddbb3b56841a6dcd92c
Yuri Eduardo Lencio <yurilencio@ciandt.com>
2026-05-05 16:29:06 -0300

    botões editar e pesquisar corrigidos

--- frontend/src/pages/home/App.jsx
+++ frontend/src/pages/home/App.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState, useRef  } from "react";
+import { useEffect, useState, useRef, use  } from "react";
 import "./App.css";
 import Lixeira from "../../assets/lixeira.png";
 import api from "../../servers/api";
 
 function App() {
 const [users, setUsers] = useState([])
-
+const [editingId, setEditingId] = useState()
 const inputName= useRef()
 const inputAge = useRef()
 const inputEmail=useRef()
+const inputPesquisa=useRef()
 
 async function getUsers(params) {
   const usersFromApi = await api.get('/usuarios')  
@@ -24,6 +25,7 @@ async function getUsers(params) {
 
       
     })
+    limparInputs();
     getUsers();
   }
 
@@ -34,6 +36,51 @@ async function getUsers(params) {
     
     getUsers();
   }
+
+    async function pesquisaUser() {
+      
+      const usersFromApi = await api.get('/usuarios',{
+        params:{
+       name: inputPesquisa.current.value,
+      }})  
+        setUsers(usersFromApi.data);
+    }
+    
+     function prepararEdicao(user) {
+      name: inputName.current.value = user.name;
+      email: inputEmail.current.value = user.email;
+      age: inputAge.current.value = user.age;
+
+     
+      setEditingId(user.id);
+     }
+      async function editarUser() {
+            await api.put(`/usuarios/${editingId}`, {
+      name: inputName.current.value,
+      email: inputEmail.current.value,
+      age: Number(inputAge.current.value),
+      });
+
+      setEditingId(null)
+      limparInputs();
+      getUsers();
+        
+      name: inputName.current.value = "";
+      email: inputEmail.current.value = "";
+      age: inputAge.current.value = "",
+
+       getUsers();
+    }
+
+     function limparInputs() {
+    inputName.current.value = "";
+    inputEmail.current.value = "";
+    inputAge.current.value = "";
+  }
+
+
+  
+  
   useEffect(() => {
     getUsers();
   }, []);
@@ -46,7 +93,14 @@ async function getUsers(params) {
           <input name="nome" type="text" placeholder="Nome" ref={inputName} />
           <input name="email" type="text" placeholder="Email" ref={inputEmail}/>
           <input name="idade" type="number" placeholder="Idade" ref={inputAge} />
-          <button type="button" onClick={createUsers}>cadastrar</button>
+          <button type="button" onClick={editingId ? editarUser : createUsers}>{editingId ? "Salvar edição" : "Cadastrar"}</button>
+          
+          <input type="text" name="pesquisa" placeholder="Pesquisar" ref={inputPesquisa} />
+          <button type="button" onClick={pesquisaUser}>Pesquisar</button>
+
+
+
+
         </form>
 
         {users.map((user) => (
@@ -56,8 +110,9 @@ async function getUsers(params) {
               <p>Email: {user.email}</p>
               <p>Idade: {user.age}</p>
             </div>
-
+            <button type="button" onClick={() => prepararEdicao(user)}>Editar</button>
             <button type="button" onClick={() => deleteUsers(user.id)}>
+              
               <img className="icone-lixeira" src={Lixeira} alt="Excluir" />
             </button>
           </div>
